92b758517a4aefb07c43b41eb501b50c4798ab77
EvanQube <evanenev@outlook.com>
2020-10-15 15:18:15 +0300

    Added command handler and ping command

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+const fs = require('fs');
 const Discord = require("discord.js");
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 prefix = "/";
 client.login(process.env.token);
 
-client.on('ready', async() => {
+client.on('ready', async () => {
   console.log('Bot is ready !')
   client.user.setPresence({
     status: 'online',
     activity: {
-        name: '/help | Автор - Evan🎃#6456',
-        type: 'WATCHING'
+      name: prefix + 'help | Автор - Evan🎃#6456',
+      type: 'WATCHING'
     }
-})
+  })
+});
+
+client.on('message', msg =>{
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(msg, args);
+    }
 });
