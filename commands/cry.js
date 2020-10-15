d27f55888d53bf8b56818bf4834add2265dc543e
EvanQube <evanenev@outlook.com>
2020-10-16 00:01:22 +0300

    Minor cry update

--- commands/cry.js
+++ commands/cry.js
@@ -2,15 +2,12 @@ module.exports = {
   name: 'cry',
   description: 'Плач',
   execute(msg, args, ) {
-    if (!msg.mentions.users.size) return msg.reply('нужно отметить пользователся, которого вы хотите поцеловать.')
-    else {
-      author = msg.author.id;
-      cry = 11;
-      cryNumber = Math.floor(Math.random() * (cry - 1)) + 1;
-      msg.channel.send('<@' + author + '>' + '\xa0' + 'плачет', {
-        files: ['./src/crys/' + cryNumber + '.gif']
-      })
-    }
-    msg.delete().catch();
+    author = msg.author.id;
+    cry = 11;
+    cryNumber = Math.floor(Math.random() * (cry - 1)) + 1;
+    msg.channel.send('<@' + author + '>' + '\xa0' + 'плачет', {
+      files: ['./src/crys/' + cryNumber + '.gif']
+    })
   }
+  msg.delete().catch();
 }
