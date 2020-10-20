4cd9d6b8d1e6be7d319aec6eb79bf2e773493a82
EvanQube <evanenev@utlookcom>
2020-10-21 00:00:28 +0300

    Minor 'mute' command bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -9,7 +9,7 @@ module.exports = {
             let role = msg.guild.roles.cache.find(role => role.name === "Muted");
 
             if (!role) {
-              msg.guild.roles.create({
+              let role = msg.guild.roles.create({
   data: {
     name: 'MUTED',
     color: '#808080',
