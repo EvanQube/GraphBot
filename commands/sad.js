b28b053c61e82ec57719268311ecd77ad528f0e1
EvanQube <evanenev@utlookcom>
2020-10-20 23:04:11 +0300

    Minor syntaxis update

--- commands/sad.js
+++ commands/sad.js
@@ -4,7 +4,7 @@ module.exports = {
   execute(msg) {
     author = msg.author.id;
     sad = 4;
-    sadhNumber = Math.floor(Math.random() * (sad - 1)) + 1;
+    sadNumber = Math.floor(Math.random() * (sad - 1)) + 1;
     msg.channel.send('<@' + author + '> грустит', {
       files: ['./src/sads/' + sadNumber + '.gif']
     })
