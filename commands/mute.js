f2291a10334137d1d257c72ea62845b2263384df
EvanQube <evanenev@utlookcom>
2020-10-21 11:57:43 +0300

    Minor 'mute' command bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -2,32 +2,32 @@ module.exports = {
   name: 'mute',
   description: "This is a mute command!",
   execute(msg, ms, args, client) {
-var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-    if(!person) return  message.reply("I CANT FIND THE USER " + person)
+var person  = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[1]));
+    if(!person) return  msg.reply("I CANT FIND THE USER " + person)
 
-    let mainrole = message.guild.roles.find(role => role.name === "Newbie");
-    let role = message.guild.roles.find(role => role.name === "mute");
+    let mainrole = msg.guild.roles.find(role => role.name === "Newbie");
+    let role = msg.guild.roles.find(role => role.name === "mute");
 
 
-    if(!role) return message.reply("Couldn't find the mute role.")
+    if(!role) return msg.reply("Couldn't find the mute role.")
 
 
     let time = args[2];
     if(!time){
-        return message.reply("You didnt specify a time!");
+        return msg.reply("You didnt specify a time!");
     }
 
     person.addRole(role.id);
 
 
-    message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
+    msg.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
 
     setTimeout(function(){
 
         person.addRole(mainrole.id)
         person.removeRole(role.id);
         console.log(role.id)
-        message.channel.send(`@${person.user.tag} has been unmuted.`)
+        msg.channel.send(`@${person.user.tag} has been unmuted.`)
     }, ms(time));
 
         msg.delete().catch();
