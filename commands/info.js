6986b18d7231808818bc97303c3ff7d7c0ce8ed2
EvanQube <evanenev@outlook.com>
2020-10-26 16:27:29 +0300

    Major 'info' command update

--- commands/info.js
+++ commands/info.js
@@ -4,13 +4,15 @@ module.exports = {
   execute(msg, Discord, prefix, client) {
     const infoEmbed = new Discord.MessageEmbed()
       .setColor('#ADD8E6')
-      .setTitle(`**Я уже на ${client.guilds.cache.size} серверах**`)
+      .setTitle('**Привет!**')
       .setAuthor('GraphBot', 'https://i.imgur.com/SY6e3l3.jpg', 'https://discord.gg/683xBMK')
       .addFields({
-        name: 'Информация',
-        value:'Бот находится в стадии разработки, предназначен для отыгрывания рп прямо в дискорде. \n' +
-        'Приглашайте бота на свои сервера, ссылку можно получить, использовав команду /invite',
-      }, )
+        name: '\u200B',
+        value:'Меня зовут **Граф**, и я предназначен для отыгрывания рп прямо в дискорде! \n' +
+        'Я поддерживаю довольно много команд, которые часто обновляются и добавляются. \n' +
+        'Ты также можешь добавить меня к себе, просто напиши команду ' + prefix + '**invite** \n' +
+        '`**Я уже на ${client.guilds.cache.size} серверах**`',
+      },)
       .setImage('https://i.pinimg.com/originals/fb/24/3f/fb243f7b97edf6267a29b99a772297da.jpg')
       .setTimestamp()
       .setFooter('Ламповый бот для рп!', 'https://i.imgur.com/SY6e3l3.jpg');
