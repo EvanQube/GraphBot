cb417983cb7418af137ee633b86a33e1ef1d1704
EvanQube <evanenev@outlook.com>
2020-10-28 20:04:57 +0300

    'Cat' command bug fix

--- commands/cat.js
+++ commands/cat.js
@@ -3,6 +3,7 @@ module.exports = {
   description: "This is a ping command!",
   execute(msg, client) {
     get('https://aws.random.cat/meow').then(res => {
-				return message.channel.send({files: [{attachment: res.body.file, name: `cat.${res.body.file.split('.')[2]}`}]});
+				return message.channel.send(
+          {files: [{attachment: res.body.file, name: `cat.${res.body.file.split('.')[2]}`}]});
   }
-}
+}}
