ac07e43b307c671174f2f983c6b47f2c7aa38e3a
EvanQube <evanenev@outlook.com>
2020-10-28 08:21:23 +0300

    Minor bug fix

--- commands/sad.js
+++ commands/sad.js
@@ -8,7 +8,7 @@ module.exports = {
     var sadNames = ['грустит', 'опечален'];
     var sads = Math.floor(Math.random() * sadNames.length);
     sadNumber = Math.floor(Math.random() * (sad - 1)) + 1;
-    msg.channel.send('<@' + author + '> ' + sads, {
+    msg.channel.send('<@' + author + '> ' + sadNames[sads], {
       files: ['./src/sads/' + sadNumber + '.gif']
     })
     msg.delete().catch();
