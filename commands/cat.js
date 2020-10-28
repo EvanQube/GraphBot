446ea5d752a5943f56558416dcb8f41a35cc7d92
EvanQube <evanenev@outlook.com>
2020-10-28 20:02:48 +0300

    'Cat' command test

---
+++ commands/cat.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ping',
+  description: "This is a ping command!",
+  execute(msg, client) {
+    get('https://aws.random.cat/meow').then(res => {
+				return message.channel.send({files: [{attachment: res.body.file, name: `cat.${res.body.file.split('.')[2]}`}]});
+  }
+}
