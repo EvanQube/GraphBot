55b2115ad74506263138e0fc43b25aee44e882f6
EvanQube <evanenev@outlook.com>
2020-10-29 12:41:20 +0300

    Bug fix

--- commands/hug.js
+++ commands/hug.js
@@ -7,17 +7,18 @@ module.exports = {
       author = msg.author.id;
       hugs = 14;
       hugsNumber = Math.floor(Math.random() * (hugs - 1)) + 1;
-      if (!msg.mentions.users.size) {
+      if (args[0] === 'everyone') {
+        msg.channel.send(`<@${author}> обнимает **ВСЕХ**`, {
+          files: ['./src/hugs/' + hugsNumber + '.gif']
+        })
+      }
+      else if (!msg.mentions.users.size) {
         let errorEmbed = {
           color: 'RED',
           description: '⛔ Ошибка\n' + 'Вы должны упомянуть пользователя, которого хотите обнять.',
         }
         msg.channel.send({
           embed: errorEmbed
-        })
-      } else if (args[0] === 'everyone') {
-        msg.channel.send(`<@${author}> обнимает **ВСЕХ**`, {
-          files: ['./src/hugs/' + hugsNumber + '.gif']
         })
       } else {
         msg.channel.send('<@' + author + '>' + '\xa0' + 'обнимает' + '\xa0' + '<@' + user.id + '>', {
