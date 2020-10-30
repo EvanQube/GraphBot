b5af0b21f474f158ae9efdb77b799a6c74c7f7d5
EvanQube <evanenev@outlook.com>
2020-10-30 22:33:38 +0300

    Bug fix

--- commands/kick.js
+++ commands/kick.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(msg, args, reason) {
       const member = msg.mentions.members.first();
       if (msg.member.hasPermission("KICK_MEMBERS")) {
-           msg.mentions.members.first().kick(reason);
+           msg.mentions.users.first().kick(reason);
         }
       }
     }
