9760978de254dd10c0d8539f6cdcbbf1f5866a39
EvanQube <evanenev@outlook.com>
2020-11-06 18:29:40 +0300

    Bug fix

--- commands/ban.js
+++ commands/ban.js
@@ -4,10 +4,10 @@ module.exports = {
   execute(msg, args) {
       if (msg.member.hasPermission("BAN_MEMBERS")) {
            let reason = args.slice(1).join(' ');
-           let user = msg.mentions.users.first();
-           let target = msg.guild.members.cache.get(user.id);
+           let target = msg.mentions.users.first();
+           let targetMember = msg.guild.members.cache.get(target.id);
            if(!user) {user = args[0]};
-           target.ban({reason: reason});
+           targetMember.ban({reason: reason});
            msg.channel.send(`<@${user.id}> был забанен!`)
         }
     }
