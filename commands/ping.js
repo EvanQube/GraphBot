65cce636d83611e231a54711b298fa922293a739
EvanQube <evanenev@outlook.com>
2020-11-06 10:29:05 +0300

    Big fix

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,13 @@
 module.exports = {
   description: 'Ping'
-  callback: (msg, args) => {
+  callback: (message) => {
     let pingEmbed = {
       color: '#7CB9E8',
       description: '🏓 pong! \n' + `Пинг: ${Math.round(client.ws.ping)}`,
       timestamp: new Date(),
     }
-    msg.channel.send({
+    message.channel.send({
       embed: pingEmbed
     })
-}
+  }
 }
