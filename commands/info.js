735886a92ebce2053ebe88de5126411644100409
EvanQube <evanenev@outlook.com>
2020-11-12 14:47:05 +0300

    Minor 'info' update

--- commands/info.js
+++ commands/info.js
@@ -5,12 +5,14 @@ module.exports = {
     const infoEmbed = new Discord.MessageEmbed()
       .setColor('#ADD8E6')
       .setTitle('**Привет!**')
-      .setAuthor('GraphBot', 'https://i.imgur.com/SY6e3l3.jpg', 'https://discord.gg/683xBMK')
+      .setAuthor('GraphBot', 'https://i.imgur.com/SY6e3l3.jpg')
+      .setDescription('Меня зовут **Граф**, я предназначен для отыгрывания рп прямо в дискорде!')
       .addFields({
         name: '\u200B',
-        value: 'Меня зовут **Граф**, я предназначен для отыгрывания рп прямо в дискорде! \n \n' +
+        value:
           'Я поддерживаю довольно много команд, которые часто обновляются и добавляются. \n' +
-          'Ты также можешь добавить меня к себе, просто напиши команду ' + prefix + '**invite** \n \n' +
+          `Чтобы узнать какие, пропиши ${prefix}help. \n` +
+          `Ты также можешь добавить меня к себе, просто напиши команду **${prefix}invite** \n \n` +
           `**Я уже на ${client.guilds.cache.size} серверах**`,
       }, )
       .setImage('https://i.pinimg.com/originals/fb/24/3f/fb243f7b97edf6267a29b99a772297da.jpg')
