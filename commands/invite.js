5746dc8a07201beb200e5df4fd0e62431fa29601
EvanQube <evanenev@outlook.com>
2020-11-12 09:36:23 +0300

    Minor update

--- commands/invite.js
+++ commands/invite.js
@@ -2,8 +2,9 @@ module.exports = {
   name: 'invite',
   description: "Пригласить бота",
   execute(msg) {
+    const reactionEmoji = client.emojis.cache.get('776334197916565508');
     msg.author.send('**Ссылка, чтобы пригласить бота к себе:** https://discord.com/api/oauth2/authorize?client_id=766070079577653258&permissions=402794566&scope=bot')
     msg.reply('я отправил тебе ссылку в личные сообщения')
-    .then(msg.react('✔️'))
+    .then(msg.react(reactionEmoji))
   }
 }
