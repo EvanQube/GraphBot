635bc8a30ff3373e294e63e7a8adce184c2543b4
EvanQube <evanenev@outlook.com>
2020-11-13 21:52:20 +0300

    Minor bug fix

--- commands/ban.js
+++ commands/ban.js
@@ -58,12 +58,12 @@ module.exports = {
     //check perms and bannable
     if (!msg.member.hasPermission("BAN_MEMBERS" || "ADMINISTRATOR")) return msg.channel.send(permsEmbed);
     if(!args[0]) return msg.channel.send(argsEmbed).then (msg.delete().catch());
-    if(!target) return msg.channel.send(targEmbed).then(msg.delete().catch());
+    if(!targetMember) return msg.channel.send(targEmbed).then(msg.delete().catch());
     if(targetMember.id === author) return msg.channel.send(authEmbed).then (msg.delete().catch());
-    if(!target.bannable) return msg.channel.send(errEmbed).then (msg.delete().catch());
+    if(!targetMember.bannable) return msg.channel.send(errEmbed).then (msg.delete().catch());
 
     //ban
-    target
+    targetMember
     .ban({reason: `${msg.author.tag}: ${reason}`})
     .then(msg.channel.send(banEmbed))
     .catch()
