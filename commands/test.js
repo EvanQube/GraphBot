4c2ae7bdd99df7f92414a7324d8c4ea75868a1e7
EvanQube <evanenev@outlook.com>
2020-11-13 15:45:33 +0300

    Minor update

--- commands/test.js
+++ commands/test.js
@@ -9,7 +9,7 @@ module.exports = {
     .setDescription(`✅`)
     .setColor('GREEN')
     .addFields({
-      name: `Вы были забанены на сервере ${guild}`,
+      name: `<@771706516377960469>`,
       value: reason,
       inline: true
     })
