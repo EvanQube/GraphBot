93ad0fdb1a136868f41bbbffad05881dd62ffb49
EvanQube <evanenev@outlook.com>
2020-11-13 11:06:48 +0300

    Bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -2,8 +2,75 @@ module.exports = {
   name: 'mute',
   description: 'Mute',
   execute(msg, args, Discord, ms) {
+    let time = args[1];
+    let author = msg.author.id;
+    let target = msg.mentions.users.first() || msg.guild.members.cache.get(args[0]);
+    let targetMember = msg.guild.member(target);
+    let guild = msg.guild.name;
 
-    const time = args[1];
+    //check reason
+    let reason = args.slice(1).join(' ');
+    if(!reason) {reason = 'None'}
+
+    //embeds
+    let kickPermsEmbed = new Discord.MessageEmbed()
+      .setColor('RED')
+      .setDescription('⛔ **Ошибка** \n У вас недостаточно прав для использования этой команды')
+
+    let authEmbed = new Discord.MessageEmbed()
+      .setColor('RED')
+      .setDescription('⛔ **Ошибка** \n Вы не можете замутить сами себя')
+
+    let argsEmbed = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setDescription('⛔ **Ошибка** \n Укажите пользователя, которого хотите замутить')
+
+    let targEmbed = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setDescription('⛔ **Ошибка** \n Я не могу найти этого пользователя')
+
+    let muteEmbed = new Discord.MessageEmbed()
+    .setDescription(`✅ ${target} замучен на сервере`)
+    .setColor('GREEN')
+    .addFields({
+      name: 'Модератор:',
+      value: `<@${author}>`,
+      inline: true
+    }, {
+      name: 'Время:',
+      value: time,
+      inline: true
+    })
+
+    let targetMuteEmbed = new Discord.MessageEmbed()
+    .setDescription(`Вы были замучены на сервере **${guild}**`)
+    .setColor('RED')
+    .addFields({
+      name:'Модератор:',
+      value:`<@${author}>`,
+      inline: true
+    }, {
+      name: 'Причина:',
+      value: reason,
+      inline: true
+    }, {
+      name: 'Время:',
+      value: time,
+      inline: true
+    })
+    .setTimestamp()
+
+    if (!msg.member.hasPermission("MANAGE_MESSAGES" || "ADMINISTRATOR")) return msg.channel.send(kickPermsEmbed);
+    if(!args[0]) return msg.channel.send(argsEmbed).then (msg.delete().catch());
+    if(!targetMember) return msg.channel.send(targEmbed).then (msg.delete().catch());
+    if(targetMember.id === author) return msg.channel.send(authEmbed).then (msg.delete().catch());
+
+    if(!time) {
+      targetMember.roles.add(mutedRole)
+    }
+
+
+  /*  const time = args[1];
     const target = msg.mentions.users.first();
     const mutedRole = msg.guild.roles.cache.find(
       (role) => role.name === 'Muted'
@@ -33,7 +100,7 @@ module.exports = {
         setTimeout(() => {
           target.roles.remove(mutedRole);
         }, time)
-      }
+      }*/
           msg.delete().catch();
     }
   }
