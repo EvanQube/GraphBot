917e86b67f0a27e96d90d727eac8a0316c43e02f
EvanQube <evanenev@outlook.com>
2020-11-13 12:08:32 +0300

    Minor bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -19,7 +19,7 @@ module.exports = {
       mutedRole = msg.guild.roles.cache.find(role => role.name === 'G-Muted')
     }
 
-    const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+    const channels = msg.guild.channels.cache.filter(ch => ch.type !== 'category');
     channels.forEach(channel => {
                 channel.updateOverwrite(message.guild.roles(mutedRole), {
                     SEND_MESSAGES: false,
