ab999acd2869221d273cb5b4b5915b323ff7060e
EvanQube <evanenev@outlook.com>
2020-11-13 12:55:10 +0300

    Minor bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -20,7 +20,10 @@ module.exports = {
     }
 
     const channels = msg.guild.channels.cache.filter(ch => ch.type !== 'category');
-    channels.updateOverwrite(mutedRole, {SEND_MESSAGES: false });
+    channels.overwritePermissions([{
+      id: mutedRole.id,
+      deny: ['SEND_MESSAGES']
+    }]);
     //check reason
     let reason = args.slice(1).join(' ');
     if(!reason) {reason = 'None'}
