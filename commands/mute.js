4aacb9e6c0af1368898961376b8b72b722b28763
EvanQube <evanenev@outlook.com>
2020-11-14 20:46:16 +0300

    Minor bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -84,46 +84,14 @@ module.exports = {
     if(targetMember.id === author) return msg.channel.send(authEmbed).then (msg.delete().catch());
 
     if(!time) {
-      targetMember.roles.add(mutedRole.id)
+      targetMember.roles.add(mutedRole)
     } else {
-      targetMember.roles.add(mutedRole.id)
+      targetMember.roles.add(mutedRole)
       setTimeout(() => {
-        targetMember.roles.remove(mutedRole.id);
+        targetMember.roles.remove(mutedRole);
       }, ms(args[1]))
     }
 
-
-  /*  const time = args[1];
-    const target = msg.mentions.users.first();
-    const mutedRole = msg.guild.roles.cache.find(
-      (role) => role.name === 'Muted'
-    );
-    if (!mutedRole) {
-      mutedRole = msg.guild.roles.create({
-        data: {
-          name: 'Muted',
-          color: '#808080',
-          permissions: {
-            deny: 'SEND_MESSAGES'
-          },
-        }
-      });
-    }
-
-    let errorEmbed = new Discord.MessageEmbed()
-      .setColor('RED')
-      .setDescription('⛔ Ошибка\n' + 'Вы должны упомянуть пользователя, которого хотите замутить.')
-
-
-    if (!msg.mentions.users.size) return msg.channel.send(errorEmbed);
-    if (!time) {
-        target.roles.add(mutedRole);
-      } else {
-        target.roles.add(mutedRole);
-        setTimeout(() => {
-          target.roles.remove(mutedRole);
-        }, time)
-      }*/
           msg.delete().catch();
     }
   }
