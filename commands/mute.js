8e1e7fffce0c795b14960f978be8689218794793
EvanQube <evanenev@outlook.com>
2020-11-14 21:35:21 +0300

    Bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -85,12 +85,11 @@ module.exports = {
 
     if(!time) {
       targetMember.roles.add(role.id)
-      target.send(targetMuteEmbed)
     } else {
       targetMember.roles.add(role.id)
       setTimeout(() => {
       targetMember.roles.remove(role.id)
-    }, ms(args[1]));
+    }, ms(args[1]))
     }
     target.send(targetMuteEmbed)
         msg.delete().catch();
