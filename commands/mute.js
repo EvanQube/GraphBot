3775548e5da199beaef46246ccdfb9208260ee45
EvanQube <evanenev@outlook.com>
2020-11-16 23:02:24 +0300

    Bug fix

--- commands/mute.js
+++ commands/mute.js
@@ -118,9 +118,9 @@ module.exports = {
       targetMember.roles.add(role.id)
 
       setTimeout(() => {
-        targetMember.roles.remove(role.id).catch(msg.channel.send(errMuteEmbed));
+        targetMember.roles.remove(role.id).catch(msg.channel.send(errMuteEmbed) break);
         target.send(unMuteEmbed)
-      }, timer).then(msg.channel.send(muteEmbed))
+      }, timer))
 
       msg.channel.send(muteEmbed)
     }
