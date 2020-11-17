50c1a5122a2cb1813e551ccd32197690ee5f11d5
EvanQube <evanenev@outlook.com>
2020-11-18 01:01:52 +0300

    Bug fix

--- commands/send.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'send',
-  description: 'Send',
-  execute(msg, args, Discord) {
-
-    msg.delete().catch();
-    }
-}
--- index.js
+++ index.js
@@ -33,39 +33,39 @@ client.on('message', msg => {
   const cmdName = args.shift().toLowerCase();
   const cmd = client.commands.get(cmdName)
 
-  if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  if(cmdName ==='anger') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='bite') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='coffee') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='cry') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='five') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='hand') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='help') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='hit') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='hug') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='info') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='invite') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='kill') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='kiss') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='laugh') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='pat') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='pinch') {cmd.execute(msg, args, ms, client, prefix, Discord)}
   else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-    cmd.execute(msg, Discord, prefix, client);
+  else if(cmdName ==='praise') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='sad') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='shock') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='shy') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='sleep') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='smile') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='sorry') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='tea') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='wink') {cmd.execute(msg, args, ms, client, prefix, Discord)}
     //Mod
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='ban') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='unban') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='kick') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='mute') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='unmute') {cmd.execute(msg, args, ms, client, prefix, Discord)}
 
 
-  else if(cmdName ==='ping') {cmd.execute(msg, args, ms, client, prefix, Discord)}
+  else if(cmdName ==='test') {cmd.execute(msg, args, ms, client, prefix, Discord)}
   });
