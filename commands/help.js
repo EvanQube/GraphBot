d88a7361158cffa97a8cffa2c39a7356fc70e16a
EvanQube <evanenev@outlook.com>
2020-11-22 21:32:09 +0300

    Bug fix

--- commands/help.js
+++ commands/help.js
@@ -35,25 +35,25 @@ module.exports.run = async (client, msg, args) => {
 
   const secondPage = new MessageEmbed()
   .setColor(`#90e0ef`)
-  .setTitle(`Префикс бота: *${prefix}*)
+  .setTitle(`Префикс бота: *${prefix}*`)
   .setAuthor(`GraphBot`, `https://i.imgur.com/SY6e3l3.jpg`, `https://discord.gg/683xBMK`)
   .setDescription('**2 страница**')
   .addFields(
   {name:`\u200B`, value:
-  '**/info** - актуальная информация о боте \n' +
-  '**/invite - пригласить бота к себе** \n' +
-  '**/ping** - проверка бота \n' +
-  '**/pat** - погладить \n' +
-  '**/kill** - убить \n' +
-  '**/anger** - позлиться \n' +
-  '**/bite** - укусить \n' +
-  '**/five** - дать пять'})
+  `**${prefix}info** - актуальная информация о боте \n` +
+  `**${prefix}invite - пригласить бота к себе** \n` +
+  `**${prefix}ping** - проверка бота \n` +
+  `**${prefix}pat** - погладить \n` +
+  `**${prefix}kill** - убить \n` +
+  `**${prefix}anger** - позлиться \n` +
+  `**${prefix}bite** - укусить \n` +
+  `**${prefix}five** - дать пять`})
   .setImage(`https://i.imgur.com/OYN4T0p.jpg`)
   .setFooter(`На ${client.guilds.cache.size} серверах`, `https://i.imgur.com/SY6e3l3.jpg`);
 
   const firstPage = new MessageEmbed()
   .setColor(`#90e0ef`)
-  .setTitle(`Префикс бота: *${prefix}*)
+  .setTitle(`Префикс бота: *${prefix}*`)
   .setAuthor(`GraphBot`, `https://i.imgur.com/SY6e3l3.jpg`)
   .setDescription('**1 страница**')
   .addFields(
@@ -68,29 +68,29 @@ module.exports.run = async (client, msg, args) => {
   '**/laugh** - посмеятся ', inline:true},
   {name:`\u200B`, value:`\u200B`, inline:true},
   {name:`\u200B`, value:
-  '**/praise** - похвалить   \n' +
-  '**/shy** - постесняться    \n' +
-  '**/coffee** - попить кофе одному или с кем-то  \n' +
-  '**/tea** - попить чай одному или с кем-то  \n' +
-  '**/shock** - быть в шоке   \n' +
-  '**/hug** - обнять  \n' +
-  '**/sorry** - извиниться (просто или перед кем-то) \n' +
-  '**/wink** - подмигнуть (просто или кому-то) ', inline:true} )
+  `**${prefix}praise** - похвалить   \n` +
+  `**${prefix}shy** - постесняться    \n` +
+  `**${prefix}coffee** - попить кофе одному или с кем-то  \n` +
+  `**${prefix}tea** - попить чай одному или с кем-то  \n` +
+  `**${prefix}shock** - быть в шоке   \n` +
+  `**${prefix}hug** - обнять  \n` +
+  `**${prefix}sorry** - извиниться (просто или перед кем-то) \n` +
+  `**${prefix}wink** - подмигнуть (просто или кому-то)`, inline:true} )
   .setImage(`https://i.imgur.com/OYN4T0p.jpg`)
   .setFooter(`На ${client.guilds.cache.size} серверах`, `https://i.imgur.com/SY6e3l3.jpg`);
 
   const modPage = new MessageEmbed()
   .setColor(`#90e0ef`)
-  .setTitle(`Префикс бота: *${prefix}*)`
+  .setTitle(`Префикс бота: *${prefix}*`)
   .setAuthor(`GraphBot`, `https://i.imgur.com/SY6e3l3.jpg`)
   .setDescription('**Модерация**')
   .addFields(
   {name:`\u200B`, value:
-  '**/ban** - забанить пользователся  \n' +
-  '**/unban** - разбанить пользователя   \n' +
-  '**/kick** - кикнуть пользователся \n' +
-  '**/mute** - замутить пользователя (если не указать время - навечно) \n' +
-  '**/unmute** - размутить пользователся  \n'})
+  `**${prefix}ban** - забанить пользователся  \n` +
+  `**${prefix}unban** - разбанить пользователя   \n` +
+  `**${prefix}kick** - кикнуть пользователся \n` +
+  `**${prefix}mute** - замутить пользователя (если не указать время - навечно) \n` +
+  `**${prefix}unmute** - размутить пользователся  \n`})
   .setImage(`https://i.imgur.com/OYN4T0p.jpg`)
   .setFooter(`На ${client.guilds.cache.size} серверах`, `https://i.imgur.com/SY6e3l3.jpg`);
 
