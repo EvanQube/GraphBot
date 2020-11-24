8dd050a7ad980dc670bb862963b4b174fc0379d2
EvanQube <evanenev@outlook.com>
2020-11-24 21:18:01 +0300

    Bug fix

--- commands/sorry.js
+++ commands/sorry.js
@@ -9,7 +9,7 @@ module.exports.help = {name:help.name, aliases: help.aliases}
 
 module.exports.run = async (client, msg, args, Discord) => {
 
-  let errorEmbed = new Discord.MessageEmbed()
+  let errorEmbed = new MessageEmbed()
     .setColor('RED')
     .setDescription('⛔ **Ошибка** \n Данная команда отключена на сервере')
   const data = await cmdsModel.findOne({
